110f8f616cdfaa128504ac80cf212c15bc6c2488
Sofia Lahham <lahhamsofia@gmail.com>
2026-08-23 22:00:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'timelink-v18';
+const CACHE = 'timelink-v19';
 const SHELL = ['./', './index.html', './app.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e)=>{
